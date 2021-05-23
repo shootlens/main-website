ddc84f90321573d095b5b6b14f25a439f2679f8b
shootlens <raghavendraachari7676@gmail.com>
2021-05-23 18:09:31 +0530

    initial commit

---
+++ photography images/image dynamic.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html>
+<body>
+	<form action="" method="post" enctype="multipart/form-data">
+		<input type="file" name="upload file" value=""/>
+		<input type="submit" name="submit" value="upload file"/>
+	</form>
+</body>
+</html>
